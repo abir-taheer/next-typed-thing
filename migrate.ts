5798fc2c2b2431443f75ed5be504c0f61dde4d7d
Abir Taheer <abir@taheer.me>
2022-02-24 05:07:49 -0500

    fixed migration script

--- migrate.ts
+++ migrate.ts
@@ -2,12 +2,4 @@ import umzug from "./database/umzug";
 
 require("ts-node/register");
 
-umzug
-  .up()
-  .then(() => {
-    console.log("All migrations completed successfully.");
-  })
-  .catch((er) => {
-    console.error(er);
-    process.exit(1);
-  });
+umzug.runAsCLI();
